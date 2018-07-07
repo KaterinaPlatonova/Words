5f35677c7c5c3267ebeab650b670b9a295ade17f
Katerina <katerinaplatonova63@gmail.com>
2018-07-07 19:14:27 +0400

    Добавлены комментарии к методам

--- PlayGuess.java
+++ PlayGuess.java
@@ -16,21 +16,17 @@ public class PlayGuess {
     private String theWord;
     private String userWord;
 
-    protected String getTheWord() {
-        return theWord;
-    }
+    protected String getTheWord() { return theWord; }
 
-    protected String getUserWord() {
-        return userWord;
-    }
+    protected String getUserWord() { return userWord; }
 
-    protected String makeTheWord() {
+    protected String makeTheWord() { // метод произвольно загадывает слово
         int i = random.nextInt(26);
         theWord = words[i];
         return theWord;
     }
 
-    protected String askUserWord() {
+    protected String askUserWord() { // метод запрашивает слово у игрока
         System.out.println("Guess the word: apple, orange, lemon, banana, apricot, avocado, broccoli, carrot,\n" +
                 "            cherry, garlic, grape, melon, leak, kiwi, mango, mushroom, nut, olive, pea,\n" +
                 "            peanut, pear, pepper, pineapple, pumpkin, potato");
@@ -40,17 +36,16 @@ public class PlayGuess {
         return userWord;
     }
 
-    protected boolean compareWords() {
-        ArrayList str = new ArrayList();
+    protected boolean compareWords() { // метод сравнивает загаданное слово и введенное
+        ArrayList str = new ArrayList(); // массив для добавления побуквенно загаданного слова
         for (int i = 0; i < theWord.length(); i++) {
             if (userWord.equals(theWord)) {
                 System.out.println("Вы угадали! Спасибо за игру!");
                 break;
             } else {
                 System.out.println("Вы не угадали слово. Будет подсказка");
-
-                str.add(theWord.charAt(i));
-                System.out.println(str + "###############");
+                str.add(theWord.charAt(i)); // добавление буквы загаданного слова в массив
+                System.out.println(str + "###############"); // вывод подсказки, очередной буквы в загаданном слове
                 askUserWord();
             }
         }
